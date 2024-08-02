75bbaabfb97055dc27da2730e6dc1c24eb50d197
Ebubekir Güngör <ebubekirgungor2003@gmail.com>
2024-08-02 22:02:00 +0300

    Update products page

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,14 +1,9 @@
 "use client";
-import { useState, useEffect, ChangeEvent, FormEvent } from "react";
-import styles from "./page.module.css";
+import { useState, useEffect } from "react";
 import LayoutContainer from "@/components/LayoutContainer";
 import LayoutBox from "@/components/LayoutBox";
 import LayoutTitle from "@/components/LayoutTitle";
 import LoadingSpinner from "@/components/LoadingSpinner";
-import Dialog from "@/components/Dialog";
-import Input from "@/components/Input";
-import Button from "@/components/Button";
-import Icon from "@/components/Icon";
 import { DataTable } from "@/components/DataTable";
 
 interface Product {
@@ -43,46 +38,14 @@ export default function Products() {
   const [products, setProducts] = useState<Product[]>([]);
   const [isLoading, setLoading] = useState(true);
 
-  async function getAllProducts() {
-    await fetch("/api/products")
+  useEffect(() => {
+    fetch("/api/products")
       .then((response) => response.json())
       .then((data) => {
         setProducts(data);
         setLoading(false);
       });
-  }
-
-  useEffect(() => {
-    getAllProducts();
   }, []);
-
-  const [dialog, setDialog] = useState(false);
-  const [dialogStatus, setDialogStatus] = useState(false);
-  const [deleteProductId, setDeleteProductId] = useState<number>();
-
-  function openDialog(id: number) {
-    setDialogStatus(true);
-    setDialog(true);
-    setDeleteProductId(id);
-  }
-
-  function closeDialog() {
-    setDialogStatus(false);
-    setTimeout(() => setDialog(false), 300);
-  }
-
-  async function onSubmit(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-
-    const response = await fetch("/api/products/" + deleteProductId, {
-      method: "DELETE",
-    });
-
-    if (response.status == 200) {
-      await getAllProducts();
-      closeDialog();
-    }
-  }
 
   return (
     <LayoutContainer>
@@ -91,19 +54,7 @@ export default function Products() {
         {isLoading ? (
           <LoadingSpinner />
         ) : (
-          <DataTable columns={columns} data={products}></DataTable>
-        )}
-        {dialog && (
-          <Dialog
-            title="Delete category"
-            close={closeDialog}
-            status={dialogStatus}
-          >
-            <form onSubmit={onSubmit}>
-              <div style={{ textAlign: "center" }}>Are you sure?</div>
-              <Button>Delete</Button>
-            </form>
-          </Dialog>
+          <DataTable columns={columns} data={products} />
         )}
       </LayoutBox>
     </LayoutContainer>
