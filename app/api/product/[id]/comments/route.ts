941dde2be16cb25a5c9583bcb1b2b0ba9437e841
Ebubekir Güngör <ebubekirgungor2003@gmail.com>
2024-09-21 21:22:55 +0300

    Create comments model & route
    Create comments section in product page

---
+++ app/api/product/[id]/comments/route.ts
@@ -0,0 +1,82 @@
+export const fetchCache = "default-no-store";
+
+import prisma from "@/lib/prisma";
+import { getPublicKey } from "@/lib/keyStore";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { handleServerError } from "@/lib/errorHandler";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+import { handler } from "@/app/middleware/handler";
+import { user } from "@/app/middleware/auth";
+
+const {
+  V4: { verify },
+} = require("paseto");
+
+async function all(_req: Request, { params }: { params: { id: string } }) {
+  const product = await prisma.product.findUnique({
+    where: { id: Number(params.id) },
+    include: {
+      comments: {
+        include: {
+          author: true,
+        },
+      },
+    },
+  });
+
+  return NextResponse.json(
+    product?.comments.map((comment) => {
+      return {
+        date: comment.updated_at,
+        content: comment.content,
+        author: {
+          first_name: (comment.author.first_name[0] ?? "*") + "***",
+          last_name: (comment.author.last_name[0] ?? "*") + "***",
+        },
+      };
+    })
+  );
+}
+
+async function create(req: Request, { params }: { params: { id: string } }) {
+  const user_id = (await verify(cookies().get("token")?.value, getPublicKey()))
+    .id;
+
+  const { content } = await req.json();
+
+  try {
+    const newComment = await prisma.comment.create({
+      data: {
+        content,
+        author_id: user_id,
+        product_id: Number(params.id),
+      },
+      include: {
+        author: true,
+      },
+    });
+
+    return NextResponse.json(
+      {
+        message: "Comment created",
+        body: {
+          date: newComment.updated_at,
+          content: newComment.content,
+          author: {
+            first_name: (newComment.author.first_name[0] ?? "*") + "***",
+            last_name: (newComment.author.last_name[0] ?? "*") + "***",
+          },
+        },
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (e) {
+    return handleServerError(e as PrismaClientKnownRequestError);
+  }
+}
+
+export const GET = handler(all);
+export const POST = handler(user, create);
--- app/product/[product]/ProductView.tsx
+++ app/product/[product]/ProductView.tsx
@@ -7,6 +7,19 @@ import Button from "@/components/ui/Button";
 import { jsonFetcher } from "@/lib/fetchers";
 import { toast } from "react-toastify";
 import LoadingSpinner from "@/components/ui/LoadingSpinner";
+import Input from "@/components/ui/Input";
+
+interface Comment {
+  date: Date;
+  content: string;
+  author: User;
+}
+
+const dateOptions: Object = {
+  day: "numeric",
+  month: "numeric",
+  year: "numeric",
+};
 
 export default function ProductView({
   product,
@@ -16,8 +29,17 @@ export default function ProductView({
   isLoggedIn: boolean;
 }) {
   const [isFavorite, setIsFavorite] = useState(false);
+  const [comments, setComments] = useState<Comment[]>([]);
+
+  const [newComment, setNewComment] = useState("");
 
   useEffect(() => {
+    fetch(`/api/product/${product.id}/comments`)
+      .then((response) => response.json())
+      .then((data) => {
+        setComments(data);
+      });
+
     if (isLoggedIn) {
       fetch("/api/product/is_favorite/" + product.id)
         .then((response) => response.json())
@@ -86,92 +108,134 @@ export default function ProductView({
     }
   }
 
+  async function addComment() {
+    const response = await jsonFetcher(
+      `/api/product/${product.id}/comments`,
+      "POST",
+      { content: newComment }
+    );
+
+    if (response.status === 200) {
+      toast.success(response.message);
+      setComments((prev) => [...prev, response.body]);
+    } else {
+      toast.error(response.message);
+    }
+  }
+
   return (
     <main className={styles.box}>
       {loading ? (
         <LoadingSpinner />
       ) : (
         <>
-          <section className={styles.sliderContainer}>
-            <div
-              className={styles.slider}
-              style={{ transform: `translateX(-${slideIndex * 100}%)` }}
-              onTouchStart={onTouchStart}
-              onTouchMove={onTouchMove}
-            >
-              {product?.images.map((image, index) => (
-                <div key={index} style={{ flex: "0 0 100%" }}>
-                  <Image
-                    src={"/images/products/" + image}
-                    alt={image}
-                    width="0"
-                    height="0"
-                    sizes="35rem"
-                  />
-                </div>
-              ))}
+          <section className={styles.productContainer}>
+            <div className={styles.sliderContainer}>
+              <div
+                className={styles.slider}
+                style={{ transform: `translateX(-${slideIndex * 100}%)` }}
+                onTouchStart={onTouchStart}
+                onTouchMove={onTouchMove}
+              >
+                {product?.images.map((image, index) => (
+                  <div key={index} style={{ flex: "0 0 100%" }}>
+                    <Image
+                      src={"/images/products/" + image}
+                      alt={image}
+                      width="0"
+                      height="0"
+                      sizes="35rem"
+                    />
+                  </div>
+                ))}
+              </div>
+              {product!.images!.length > 1 && (
+                <>
+                  <button
+                    style={{ left: "1rem" }}
+                    className={styles.slideButton}
+                    type="button"
+                    onClick={() => gotoSlide(slideIndex - 1)}
+                  >
+                    <Icon name="previous" />
+                  </button>
+                  <button
+                    style={{ left: "calc(100% - (40px + 1rem))" }}
+                    className={styles.slideButton}
+                    type="button"
+                    onClick={() => gotoSlide(slideIndex + 1)}
+                  >
+                    <Icon name="next" />
+                  </button>
+                </>
+              )}
+              <div className={styles.miniImages}>
+                {product?.images.map((image, index) => (
+                  <button
+                    className={slideIndex === index ? styles.selected : ""}
+                    key={index}
+                    type="button"
+                    onClick={() => gotoSlide(index)}
+                  >
+                    <Image
+                      src={"/images/products/" + image}
+                      alt={image}
+                      width="0"
+                      height="0"
+                      sizes="3rem"
+                    />
+                  </button>
+                ))}
+              </div>
             </div>
-            {product!.images!.length > 1 && (
-              <>
-                <button
-                  style={{ left: "1rem" }}
-                  className={styles.slideButton}
-                  type="button"
-                  onClick={() => gotoSlide(slideIndex - 1)}
-                >
-                  <Icon name="previous" />
-                </button>
-                <button
-                  style={{ left: "calc(100% - (40px + 1rem))" }}
-                  className={styles.slideButton}
-                  type="button"
-                  onClick={() => gotoSlide(slideIndex + 1)}
-                >
-                  <Icon name="next" />
-                </button>
-              </>
-            )}
-            <div className={styles.miniImages}>
-              {product?.images.map((image, index) => (
-                <button
-                  className={slideIndex === index ? styles.selected : ""}
-                  key={index}
-                  type="button"
-                  onClick={() => gotoSlide(index)}
-                >
-                  <Image
-                    src={"/images/products/" + image}
-                    alt={image}
-                    width="0"
-                    height="0"
-                    sizes="3rem"
-                  />
-                </button>
-              ))}
+            <div className={styles.product}>
+              <div className={styles.title}>
+                {product?.title}
+                {isLoggedIn && (
+                  <button
+                    className={styles.favoriteButton}
+                    onClick={toggleFavorite}
+                  >
+                    {isFavorite ? (
+                      <Icon name="favorite_filled" disableFilter />
+                    ) : (
+                      <Icon name="favorite" />
+                    )}
+                  </button>
+                )}
+              </div>
+              <div className={styles.priceAndButton}>
+                <div className={styles.listPrice}>
+                  {product?.list_price.toLocaleString("tr-TR")} TL
+                </div>
+                <Button onClick={addProductToCart}>Add to Cart</Button>
+              </div>
             </div>
           </section>
-          <section className={styles.product}>
-            <div className={styles.title}>
-              {product?.title}
-              {isLoggedIn && (
-                <button
-                  className={styles.favoriteButton}
-                  onClick={toggleFavorite}
-                >
-                  {isFavorite ? (
-                    <Icon name="favorite_filled" disableFilter />
-                  ) : (
-                    <Icon name="favorite" />
-                  )}
-                </button>
-              )}
+          <section className={styles.commentsContainer}>
+            <div className={styles.comment}>
+              <Input
+                value={newComment}
+                onChange={(e) => setNewComment(e.target.value)}
+              />
+              <Button onClick={addComment}>Create</Button>
             </div>
-            <div className={styles.priceAndButton}>
-              <div className={styles.listPrice}>
-                {product?.list_price.toLocaleString("tr-TR")} TL
+            {comments.map((comment, index) => (
+              <div className={styles.comment} key={index}>
+                <div className={styles.spaceBetween}>
+                  <span>{comment.content}</span>
+                  <span>
+                    {new Date(comment.date).toLocaleString(
+                      "tr-TR",
+                      dateOptions
+                    )}
+                  </span>
+                </div>
+                <div className={styles.author}>
+                  {comment.author.first_name} {comment.author.last_name}
+                </div>
               </div>
-              <Button onClick={addProductToCart}>Add to Cart</Button>
-            </div>
+            ))}
           </section>
         </>
       )}
