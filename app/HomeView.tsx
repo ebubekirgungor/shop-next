b6373dc09ef080edcdf34ba39884c3e6922f696d
Ebubekir Güngör <ebubekirgungor2003@gmail.com>
2024-09-25 01:24:36 +0300

    Make home view responsive
    Fix category page filter box height

--- app/HomeView.tsx
+++ app/HomeView.tsx
@@ -4,15 +4,19 @@ import styles from "./page.module.css";
 import Link from "next/link";
 
 export default function HomePage({ categories }: { categories: Category[] }) {
-  return categories.map((category) => (
-    <Link href={category.url} className={styles.category} key={category.id}>
-      <Image
-        src={category.image!}
-        alt={category.image!}
-        width="0"
-        height="0"
-        sizes="18rem"
-      />
-    </Link>
-  ));
+  return (
+    <section className={styles.categoryContainer}>
+      {categories.map((category) => (
+        <Link href={category.url} className={styles.category} key={category.id}>
+          <Image
+            src={category.image!}
+            alt={category.image!}
+            width="0"
+            height="0"
+            sizes="18rem"
+          />
+        </Link>
+      ))}
+    </section>
+  );
 }
